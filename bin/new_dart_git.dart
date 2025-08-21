a723b1db05c41b6da6f04edbd5d2c71251ff74e5
6631501045-TBuaekem <6631501045@lamduan.mfu.ac.th>
2025-08-21 16:59:53 +0700

    Feature sum worked correctly

--- bin/new_dart_git.dart
+++ bin/new_dart_git.dart
@@ -2,10 +2,10 @@
 void sum(int one, int two){
   //dev 1 pls do function sum  
   int sum = one + two;
-  print('Sun: $sum');
+  print('Su: $sum');
 }
 
 void main(){
   print("Sum Function Example");
-
+  sum(1, 2);
 }
